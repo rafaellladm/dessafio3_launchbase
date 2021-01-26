78af3f8b74f7674719a25602619ba1c5a85641c9
Rafael Luz <rafaellladm@gmail.com>
2021-01-25 23:58:33 -0300

    criaçao de data para alimentar DOM

---
+++ data.js
@@ -0,0 +1,20 @@
+module.exports = [
+    {
+        id: "starter",
+        title: "Torne-se um programador desejado",
+        n_modulo: 5,
+        price: "Gratuito"
+    },
+    {
+        id: "launchbase",
+        title: "Domine programação do zero",
+        n_modulo: 16,
+        price: "Pago"
+    },
+    {
+        id: "gostack",
+        title: "Treinamento imersivo",
+        n_modulo: 20,
+        price: "Pago"
+    }
+]
