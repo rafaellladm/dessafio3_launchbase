98a7883e67d23f298791e5bd965149aee724dfce
Rafael Luz <rafaellladm@gmail.com>
2021-02-04 23:49:54 -0300

    modificaçao de obijetos do array

--- data.js
+++ data.js
@@ -1,19 +1,22 @@
 module.exports = [
     {
         id: "starter",
-        title: "Torne-se um programador desejado",
+        title: "Starter",
+        description: "Torne-se um programador desejado",
         n_modulo: 5,
         price: "Gratuito"
     },
     {
         id: "launchbase",
-        title: "Domine programação do zero",
+        title: "LaunchBase",
+        description: "Domine programação do zero",
         n_modulo: 16,
         price: "Pago"
     },
     {
         id: "gostack",
-        title: "Treinamento imersivo",
+        title: "GoStack",
+        description: "Treinamento imersivo",
         n_modulo: 20,
         price: "Pago"
     }
