2f79d33961fa49f51624fe8ae0a57d64b13f69ac
Rafael Luz <rafaellladm@gmail.com>
2021-02-04 23:51:14 -0300

    modificaçao de abertura de page no moral para pagina

--- public/script.js
+++ public/script.js
@@ -6,29 +6,30 @@ const modal = document.querySelector(".modal")
 for (let card of cards) {
     card.addEventListener("click", function(){
 
-        // ativação do modal com clique no card
-        modalOverlay.classList.add('active')
+        // // ativação do modal com clique no card
+        // modalOverlay.classList.add('active')
 
         // criação de url mudável com clique em card
         const pageId = card.getAttribute('id')
-        modalOverlay.querySelector('iframe').src = `https://rocketseat.com.br/${pageId}`
+        window.location.href = `/courses/${pageId}`
+        // modalOverlay.querySelector('iframe').src = `https://rocketseat.com.br/${pageId}`
 
     })
 }
 
-// encerramento do modal
-document.querySelector(".close-modal").addEventListener("click", function(){
-    modalOverlay.classList.remove('active')
-    modal.classList.remove('maximize')
-})
+// // encerramento do modal
+// document.querySelector(".close-modal").addEventListener("click", function(){
+//     modalOverlay.classList.remove('active')
+//     modal.classList.remove('maximize')
+// })
 
-// maximizar modal
-document.querySelector(".maximize-modal").addEventListener("click", function(){
+// // maximizar modal
+// document.querySelector(".maximize-modal").addEventListener("click", function(){
     
-    // utilizando o método contains
-    if (modal.classList.contains('maximize')) {
-        modal.classList.remove('maximize')
-    } else {
-        modal.classList.add('maximize')
-    }
-})
+//     // utilizando o método contains
+//     if (modal.classList.contains('maximize')) {
+//         modal.classList.remove('maximize')
+//     } else {
+//         modal.classList.add('maximize')
+//     }
+// })
